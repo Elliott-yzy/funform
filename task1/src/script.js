3f67d1289c791b150324a2d00d4e5480522cd86b
杨赵艺 <yangzhaoyi@YZYMacBook-Pro.local>
2024-09-26 11:30:41 +1000

    merge the functions to improve the coherence

--- task1/src/script.js
+++ task1/src/script.js
@@ -9,28 +9,47 @@ const other_course_input = document.getElementById('other');
 const reset_button = document.getElementById('resetButton');
 const output_text = document.getElementById('outputText');
 
-const fun_fullname = (fullName) => {
-    if (fullName.length < 3 || fullName.length > 50) {
-        output_text.value = 'Please input a valid full name';
-        return false;
-    }
-    return true;
-};
-input_fullname.addEventListener('blur', () => fun_fullname(input_fullname.value));
+const all_input = () => {
+  const for_fullname = input_fullname.value.trim();
+  const for_birth = input_birth.value.trim();
+  const for_graduationDate = input_graduate.value;
 
-const fun_birth = (dob) => {
-    const regex_function = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
-    if (!regex_function.test(dob)) {
-        output_text.value = 'Please input a valid date of birth';
-        return false;
-    }
-    const [day, month, year] = dob.split('/').map(Number);
-    const user_birth = new Date(year, month - 1, day);
-    const current_date = new Date();
-    if (user_birth.getDate() !== day || user_birth.getMonth() + 1 !== month || user_birth >= current_date) {
-        output_text.value = 'Please input a valid date of birth';
-        return false;
-    }
-    return user_birth;
-};
-input_birth.addEventListener('blur', () => fun_birth(input_birth.value));
+  if (for_fullname.length < 3 || for_fullname.length > 50) {
+    output_text.value = 'Please input a valid full name';
+    return;
+  }
+
+  const regex_function = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+  if (!regex_function.test(for_birth)) {
+    output_text.value = 'Please input a valid date of birth';
+    return;
+  }
+  const [day, month, year] = for_birth.split('/').map(Number);
+  const user_birth = new Date(year, month - 1, day);
+  const current_date = new Date();
+  if (user_birth.getDate() !== day || user_birth.getMonth() + 1 !== month || user_birth >= current_date) {
+    output_text.value = 'Please input a valid date of birth';
+    return;
+  }
+}
+
+input_fullname.addEventListener('blur', all_input);
+input_birth.addEventListener('blur', all_input);
+
+
+
+// const fun_graduate = (graduationDate) => {
+//   if (!graduationDate) {
+//     output_text.value = 'Please input a valid graduation date';
+//     return false;
+//   }
+//   const [year, month, day] = graduationDate.split('/').map(Number);
+//   const user_graduationDate = new Date(year, month - 1, day);
+//   const current_date = new Date();
+//   if (user_graduationDate <= current_date) {
+//     output_text.value = 'Please input a valid date of birth';
+//     return false;
+//   }
+//   return user_graduationDate;
+// }
+// input_graduate.addEventListener('blur', () => fun_graduate(input_graduate.value))
